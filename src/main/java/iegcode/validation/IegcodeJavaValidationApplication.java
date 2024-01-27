b1e6454154f6a0f628a359c23bb690de01e476f1
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-27 23:33:11 +0700

    first commit

---
+++ src/main/java/iegcode/validation/IegcodeJavaValidationApplication.java
@@ -0,0 +1,13 @@
+package iegcode.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IegcodeJavaValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IegcodeJavaValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iegcode/validation/IegcodeJavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IegcodeJavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
